87087054bda668bc9c14d479fe7befeb3169b225
Obianuju Jonathan <ObianujuJonathan@gmail.com>
2022-09-20 22:44:54 +0100

    todo-app

--- script/index.js
+++ script/index.js
@@ -1,5 +1,4 @@
 
-// wait for page to load
 window.addEventListener('load', () => {
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
